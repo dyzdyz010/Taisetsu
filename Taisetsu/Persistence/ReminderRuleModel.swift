ae03ec6d750374baac50615d8c321ff3706ed0b4
Etern1ty <dyzdyz010@gmail.com>
2026-08-08 01:01:29 +0800

    feat: persist custom reminder times

--- Taisetsu/Persistence/ReminderRuleModel.swift
+++ Taisetsu/Persistence/ReminderRuleModel.swift
@@ -5,12 +5,19 @@ import SwiftData
 final class ReminderRuleModel {
     var id: UUID = UUID()
     var offsetMinutes: Int = 0
+    var timeOfDayMinutes: Int?
     var isEnabled: Bool = true
     var anniversary: AnniversaryModel?
 
-    init(id: UUID = UUID(), offsetMinutes: Int, isEnabled: Bool = true) {
+    init(
+        id: UUID = UUID(),
+        offsetMinutes: Int,
+        timeOfDayMinutes: Int? = nil,
+        isEnabled: Bool = true
+    ) {
         self.id = id
         self.offsetMinutes = offsetMinutes
+        self.timeOfDayMinutes = timeOfDayMinutes
         self.isEnabled = isEnabled
     }
 }
