e028cb02575372206a32b7e90e482202a086b347
Etern1ty <dyzdyz010@gmail.com>
2026-08-08 01:01:20 +0800

    feat: support custom same-day reminder times

--- TaisetsuCore/Domain/ReminderSpec.swift
+++ TaisetsuCore/Domain/ReminderSpec.swift
@@ -3,11 +3,18 @@ import Foundation
 public struct ReminderSpec: Codable, Equatable, Identifiable, Sendable {
     public let id: UUID
     public let offsetMinutes: Int
+    public let timeOfDayMinutes: Int?
     public let isEnabled: Bool
 
-    public init(id: UUID = UUID(), offsetMinutes: Int, isEnabled: Bool = true) {
+    public init(
+        id: UUID = UUID(),
+        offsetMinutes: Int,
+        timeOfDayMinutes: Int? = nil,
+        isEnabled: Bool = true
+    ) {
         self.id = id
         self.offsetMinutes = offsetMinutes
+        self.timeOfDayMinutes = timeOfDayMinutes
         self.isEnabled = isEnabled
     }
 }
